5d737adbb61b714fac54f50c4db126b586118484
tatiane <contatotatianegardin@hotmail.com>
2022-02-28 17:20:42 -0300

    subindo mesa de trabalho

---
+++ mesa_aula3/calculadora.js
@@ -0,0 +1,7 @@
+const somar = require('./funcaoSomar.js')
+const subtrair = require('./funcaoSomar.js')
+
+console.log(somar(2,0))
+
+
+
---
+++ mesa_aula3/funcaoSomar.js
@@ -0,0 +1,10 @@
+function somar(a,b){
+    if( a == 0 || b == 0){
+        console.log("não se pode dividir por zero")
+    }
+       
+}
+
+
+
+module.exports = somar;
