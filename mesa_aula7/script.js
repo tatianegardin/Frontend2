9656519783b43c88177b1682696431d854973ad6
tatiane <contatotatianegardin@hotmail.com>
2022-03-11 17:57:53 -0300

    subindo atividades

---
+++ mesa_aula7/script.js
@@ -0,0 +1,36 @@
+let valor = prompt('Digite o nome de um animal');
+let url = prompt('coloque sua url');
+let div = document.querySelectorAll('.item');
+let card 
+
+valor = valor.toUpperCase()
+
+switch (valor) {
+    case 'TIGRE':
+        card = div[0].children[0];
+        break;
+    case 'LEÃO':
+        card = div[1].children[0];
+        break;
+    case 'LEAO':
+        card = div[1].children[0];
+        break;
+    case 'LEOPARDO':
+        card = div[2].children[0];
+        break;
+    case 'PANTERA NEGRA':
+        card = div[3].children[0];
+        break;
+    case 'JAGUAR':
+        card = div[4].children[0];
+        break;
+    case 'GUEPARDO':
+        card = div[5].children[0];
+    break;
+    default:
+        alert('Atualize a página e tente novamente!')
+        break;
+}
+
+card.removeAttribute('src');
+card.setAttribute('src', url);
