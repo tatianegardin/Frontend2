08b0f151fe8f7fa4cc950ad95b6879e70194706d
tatiane <contatotatianegardin@hotmail.com>
2022-03-24 19:54:32 -0300

    subindo mesa aula 13

---
+++ mesa_aula13/conteudo/script.js
@@ -0,0 +1,144 @@
+// capturando INPUT
+let inputNome = capturaID('nome');
+let inputIdade = capturaID('idade');
+let inputAltura = capturaID('altura');
+let inputEmail = capturaID('email');
+let inputBiografia = capturaID('biografia');
+
+// capturando tags ppara futuros campos que serão subscritos
+let campoNome = capturaID('nomeUsuario');
+let campoSobrenome = capturaID('sobrenomeUsuario');
+let campoIdade = capturaID('idadeUsuario');
+let campoAltura = capturaID('alturaUsuario');
+let campoEmail = capturaID('emailUsuario');
+let campoBiografia = capturaID('biografiaUsuario');
+
+//capturando campos para informação de erro ao preenchimento input (tag small)
+let validacaoNome = capturaID('nomeValidacao');
+let validacaoEmail = capturaID('emailValidacao');
+let validacaoIdade = capturaID('idadeValidacao')
+let validacaoAltura = capturaID('alturaValidacao')
+let validacaoBiografia  = capturaID('biografiaValidacao')
+
+//capturando botao salvar
+let botaoSalvar = document.getElementById('salvar')
+
+//criação objeto 
+let usuarioObjeto = {
+    nome: '',
+    sobrenome: '',
+    email: '',
+    idade: '',
+    altura: '',
+    biografia: '',
+}
+
+//function para capturar id
+function capturaID(id){
+    return document.getElementById(id);
+}
+
+
+// function mostrar na tela
+function alterarCampos(){
+    let objetoDoStorage = pegaObjetoStorage()
+    campoNome.innerHTML = objetoDoStorage.nome;
+    campoSobrenome.innerHTML = objetoDoStorage.sobrenome;
+    campoIdade.innerHTML = objetoDoStorage.idade;
+    campoAltura.innerHTML = objetoDoStorage.altura;
+    campoEmail.innerHTML = objetoDoStorage.email;
+    campoBiografia.innerHTML = objetoDoStorage.biografia ;
+}
+
+//fuunção adicionar valores ao objeto
+function adicionarAoObjeto(){
+    let inputNomeSobrenome = inputNome.value.trim();
+    let nomeSobrenome = inputNomeSobrenome.split(' ');
+    usuarioObjeto.nome = nomeSobrenome[0];
+    usuarioObjeto.sobrenome = nomeSobrenome[1];
+    usuarioObjeto.idade = inputIdade.value;
+    usuarioObjeto.altura = inputAltura.value;
+    usuarioObjeto.email = inputEmail.value;
+    usuarioObjeto.biografia = inputBiografia.value;
+}
+
+// função limpar os inputs
+function limparInput(){
+    inputNome.value = ''
+    inputIdade.value =''
+    inputAltura.value =''
+    inputEmail.value =''
+    inputBiografia.value =''
+}
+
+//função adicionar objeto ao localstorage
+function addObjetoStorage(objeto){
+    let objetoConvertido = JSON.stringify(objeto)
+    localStorage.setItem(1, objetoConvertido)
+}
+
+//capturar objeto localstorage
+function pegaObjetoStorage(){
+    let objetoCapturado = localStorage.getItem(1)
+    return JSON.parse(objetoCapturado)
+}
+
+
+
+// function para validação de não preenchimento do input
+function validarInput(input, print){
+
+    input.addEventListener('blur', () => {
+        if(input.value != ''){
+            input.style.borderColor = '#adb5bd'
+            print.innerHTML = ''
+        }else{
+            input.style.borderColor= 'red'
+            print.innerHTML = 'Campo obrigatório'
+            print.style.color = 'red'
+            
+        }
+    })
+}
+
+
+// validação para não preenchimento do input email
+inputEmail.addEventListener('blur', () => {
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value) ){
+        inputEmail.style.borderColor = '#adb5bd'
+        validacaoEmail.innerHTML = ''
+    }else if(inputEmail.value == ''){
+        inputEmail.style.borderColor= 'red'
+        validacaoEmail.innerHTML = 'Campo obrigatório'
+        validacaoEmail.style.color = 'red'
+    }
+    else{
+        inputEmail.style.borderColor= 'red'
+        validacaoEmail.innerHTML = 'Email inválido'
+        validacaoEmail.style.color = 'red'
+        
+    }
+})
+
+// validação de input não preenchido
+validarInput(inputNome, validacaoNome);
+validarInput(inputIdade, validacaoIdade);
+validarInput(inputAltura, validacaoAltura);
+validarInput(inputBiografia, validacaoBiografia);
+
+// ao carregar a pagina é exibida das informações presentes no storage
+onload = () => {
+    alterarCampos()
+    limparInput()
+}
+
+// ao clicar no botão é alterado todas as informações
+botaoSalvar.addEventListener('click', evento => {
+    evento.preventDefault();
+    adicionarAoObjeto();
+    addObjetoStorage(usuarioObjeto);
+    alterarCampos();  
+    limparInput();
+})
+
+
--- mesa_aula13/script.js
+++ mesa_aula13/script.js
@@ -1,144 +1,57 @@
-// capturando INPUT
-let inputNome = capturaID('nome');
-let inputIdade = capturaID('idade');
-let inputAltura = capturaID('altura');
-let inputEmail = capturaID('email');
-let inputBiografia = capturaID('biografia');
+let inputNome = document.querySelector('#nome');
+let inputSobrenome = document.querySelector('#sobrenome');
+let btnSubmit = document.querySelector('#submit');
+let btnLimpar = document.querySelector('#remove');
+let h1 = document.querySelector('h1');
 
-// capturando tags ppara futuros campos que serão subscritos
-let campoNome = capturaID('nomeUsuario');
-let campoSobrenome = capturaID('sobrenomeUsuario');
-let campoIdade = capturaID('idadeUsuario');
-let campoAltura = capturaID('alturaUsuario');
-let campoEmail = capturaID('emailUsuario');
-let campoBiografia = capturaID('biografiaUsuario');
-
-//capturando campos para informação de erro ao preenchimento input (tag small)
-let validacaoNome = capturaID('nomeValidacao');
-let validacaoEmail = capturaID('emailValidacao');
-let validacaoIdade = capturaID('idadeValidacao')
-let validacaoAltura = capturaID('alturaValidacao')
-let validacaoBiografia  = capturaID('biografiaValidacao')
-
-//capturando botao salvar
-let botaoSalvar = document.getElementById('salvar')
-
-//criação objeto 
-let usuarioObjeto = {
+let objetoUsuario = {
     nome: '',
     sobrenome: '',
-    email: '',
-    idade: '',
-    altura: '',
-    biografia: '',
 }
 
-//function para capturar id
-function capturaID(id){
-    return document.getElementById(id);
-}
-
-
-// function mostrar na tela
-function alterarCampos(){
-    let objetoDoStorage = pegaObjetoStorage()
-    campoNome.innerHTML = objetoDoStorage.nome;
-    campoSobrenome.innerHTML = objetoDoStorage.sobrenome;
-    campoIdade.innerHTML = objetoDoStorage.idade;
-    campoAltura.innerHTML = objetoDoStorage.altura;
-    campoEmail.innerHTML = objetoDoStorage.email;
-    campoBiografia.innerHTML = objetoDoStorage.biografia ;
-}
-
-//fuunção adicionar valores ao objeto
 function adicionarAoObjeto(){
-    let inputNomeSobrenome = inputNome.value.trim();
-    let nomeSobrenome = inputNomeSobrenome.split(' ');
-    usuarioObjeto.nome = nomeSobrenome[0];
-    usuarioObjeto.sobrenome = nomeSobrenome[1];
-    usuarioObjeto.idade = inputIdade.value;
-    usuarioObjeto.altura = inputAltura.value;
-    usuarioObjeto.email = inputEmail.value;
-    usuarioObjeto.biografia = inputBiografia.value;
+    objetoUsuario.nome = inputNome.value
+    objetoUsuario.sobrenome = inputSobrenome.value
 }
 
-// função limpar os inputs
-function limparInput(){
-    inputNome.value = ''
-    inputIdade.value =''
-    inputAltura.value =''
-    inputEmail.value =''
-    inputBiografia.value =''
-}
-
-//função adicionar objeto ao localstorage
-function addObjetoStorage(objeto){
+function criarObjetoStorage(objeto){
     let objetoConvertido = JSON.stringify(objeto)
-    localStorage.setItem(1, objetoConvertido)
+    localStorage.setItem(1,objetoConvertido)
 }
 
-//capturar objeto localstorage
-function pegaObjetoStorage(){
+function capturarObjeto(){
     let objetoCapturado = localStorage.getItem(1)
     return JSON.parse(objetoCapturado)
 }
 
 
-
-// function para validação de não preenchimento do input
-function validarInput(input, print){
-
-    input.addEventListener('blur', () => {
-        if(input.value != ''){
-            input.style.borderColor = '#adb5bd'
-            print.innerHTML = ''
-        }else{
-            input.style.borderColor= 'red'
-            print.innerHTML = 'Campo obrigatório'
-            print.style.color = 'red'
-            
-        }
-    })
+function mostrarNaTela(){
+    let objetoStorage = capturarObjeto()
+    if(objetoStorage != null){
+        h1.innerText = `Olá, ${objetoStorage.nome} ${objetoStorage.sobrenome}!!`
+    }
 }
 
+function limparInput(){
+    inputNome.value = ''
+    inputSobrenome.value = ''
+}
 
-// validação para não preenchimento do input email
-inputEmail.addEventListener('blur', () => {
-    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value) ){
-        inputEmail.style.borderColor = '#adb5bd'
-        validacaoEmail.innerHTML = ''
-    }else if(inputEmail.value == ''){
-        inputEmail.style.borderColor= 'red'
-        validacaoEmail.innerHTML = 'Campo obrigatório'
-        validacaoEmail.style.color = 'red'
-    }
-    else{
-        inputEmail.style.borderColor= 'red'
-        validacaoEmail.innerHTML = 'Email inválido'
-        validacaoEmail.style.color = 'red'
-        
-    }
-})
+onload = mostrarNaTela
 
-// validação de input não preenchido
-validarInput(inputNome, validacaoNome);
-validarInput(inputIdade, validacaoIdade);
-validarInput(inputAltura, validacaoAltura);
-validarInput(inputBiografia, validacaoBiografia);
-
-// ao carregar a pagina é exibida das informações presentes no storage
-onload = () => {
-    alterarCampos()
+btnSubmit.addEventListener('click', event =>{
+    event.preventDefault()
+    adicionarAoObjeto()
+    criarObjetoStorage(objetoUsuario)
+    mostrarNaTela()
     limparInput()
-}
-
-// ao clicar no botão é alterado todas as informações
-botaoSalvar.addEventListener('click', evento => {
-    evento.preventDefault();
-    adicionarAoObjeto();
-    addObjetoStorage(usuarioObjeto);
-    alterarCampos();  
-    limparInput();
 })
+
+btnLimpar.addEventListener('click', evento =>{
+    evento.preventDefault()
+    localStorage.clear()
+    h1.innerHTML = ''
+})
+
 
 
